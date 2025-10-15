143a402ab3dd3f2887a1ba2d92e24001a5f6f411
Xabier <x_bilbaozam@fpzornotzalh.eus>
2025-10-15 13:53:08 +0200

    made a reacting form with the chekboxes

--- src/client/assets/js/preSending/formLoad.js
+++ src/client/assets/js/preSending/formLoad.js
@@ -1,10 +1,31 @@
 const WAREHOUSES_URL = "/src/client/assets/json/preSending/loadForm/warehouses.json"
+const DESTINATION_CHECK = document.getElementById("bidalketa")
+const DESTINATION_TF = document.getElementById("helmuga")
+
 kargatuBiltegiak(WAREHOUSES_URL, "biltegia")
+
+DESTINATION_CHECK.addEventListener("click", () => {
+
+/**
+ * if(DESTINATION_TF.disabled) {
+ *      
+ *      DESTINATION_TF.disabled = false
+ * 
+ * } else {
+ *      
+ *      DESTINATION_TF.disabled = true
+ * 
+ * } 
+ */
+
+    DESTINATION_TF.disabled = !DESTINATION_TF.disabled
+
+})
 
 async function kargatuBiltegiak(url, selectID) {
 
     try {
-        
+         
         response = await fetch(url);
         
         if (!response.ok) {
@@ -17,13 +38,16 @@ async function kargatuBiltegiak(url, selectID) {
         const select = document.getElementById(selectID)
 
         options.forEach(item => {
+            
             const option = document.createElement("option")
             option.value = item.id
             option.textContent = item.izena
             select.appendChild(option)
-            console.log("a")
+
         })
+
     } catch (e) {
+
         console.error("Ezin izan da biltegiak kargatu:", e);
     }
 }
