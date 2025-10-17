20f18a75b4ee9a9cff7db86bcc766e2085ac87a7
IzaAne <a_izaguirrelor@fpzornotzalh.eus>
2025-10-17 12:34:53 +0200

    fin

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,9 +1,52 @@
-var counter =1
+var counter = 1
+const cardTitles = document.querySelectorAll(".titulo2")
+const cardTexts = document.querySelectorAll(".text")
+var mediaQuery = window.matchMedia("(max-width: 660px)")
 
-setInterval(function() {
+console.log(cardTitles)
+setInterval(function () {
     document.getElementById('radio' + counter).checked = true
     counter++
-    if(counter > 3){
-        counter =1
+    if (counter > 3) {
+        counter = 1
     }
 }, 7000)
+
+checkMedia(mediaQuery)
+mediaQuery.addListener(checkMedia)
+
+function checkMedia(media) {
+
+    if (media.matches) {
+
+        cardTitles.forEach((item, index) => {
+
+            cardTexts[index].style.display = "none"
+
+            item.addEventListener("click", (e) => {
+
+                e.preventDefault()
+
+
+                if (cardTexts[index].style.display === "none") {
+
+                    cardTexts[index].style.display = "block"
+
+                } else {
+
+                    cardTexts[index].style.display = "none"
+
+                }
+            })
+        })
+    } else {
+
+        cardTexts.forEach(item => {
+
+            item.style.display = "block"
+        })
+
+    }
+}
+
+
