ac269f811a251f4c4b72182c81ac7d9248c3da03
XBilbaoo <x_bilbaozam@fpzornotza.com>
2025-10-22 19:13:03 +0200

    starting with some css

--- src/client/js/preSending/formLoad.js
+++ src/client/js/preSending/formLoad.js
@@ -1,8 +1,12 @@
-const WAREHOUSES_URL = "/src/client/assets/json/preSending/loadForm/warehouses.json"
-const DESTINATION_CHECK = document.getElementById("bidalketa")
-const DESTINATION_TF = document.getElementById("helmuga")
+const WAREHOUSES_URL = "/src/client/json/preSending/loadForm/warehouses.json"
 
-kargatuBiltegiak(WAREHOUSES_URL, "biltegia")
+const DESTINATION_CHECK = document.getElementById("bidalketa")
+const WAREHOUSE_CHECK = document.getElementById("biltegiratze")
+const DESTINATION_TF = document.getElementById("helmuga")
+const WAREHOUSE_SELECT = document.getElementById("biltegia")
+
+
+kargatuBiltegiak(WAREHOUSES_URL, WAREHOUSE_SELECT)
 
 DESTINATION_CHECK.addEventListener("click", () => {
 
@@ -21,6 +25,20 @@ DESTINATION_CHECK.addEventListener("click", () => {
     DESTINATION_TF.disabled = !DESTINATION_TF.disabled
 
 })
+
+WAREHOUSE_CHECK.addEventListener("click", () => {
+
+    if(WAREHOUSE_SELECT.style.display == "inline-block") {
+
+        WAREHOUSE_SELECT.style.display = "none"
+
+    } else {
+
+        WAREHOUSE_SELECT.style.display = "inline-block"
+
+    }
+})
+
 
 /**
  * Funtion to load the names of the warehouses via JSON file.
